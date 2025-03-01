f72bbf75857bc8bdf4231a02687863b25734f991
AbrahamPG <abrapaga@gmail.com>
2025-03-01 02:02:48 -0500

    Probando api web subida

--- src/app/products/features/product/product.service.ts
+++ src/app/products/features/product/product.service.ts
@@ -1,18 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class ProductService {
     
     private http = inject(HttpClient)
 
-    getListProducts():Observable<any>{
-        const url ='http://127.0.0.1:8000/productos/v1/products/'
-        
-        // 'https://djangoapi-2xnu.onrender.com/productos/v1/'
-        return this.http.get<any>(url)
-    } 
+    private apiWeb = 'https://djangoapi-2xnu.onrender.com/productos/v1/products/';
+    private apiLocal = 'http://127.0.0.1:8000/productos/v1/products/';
+
+    getListProducts(): Observable<any> {
+        return this.http.get<any>(this.apiWeb).pipe(
+            catchError(error => {
+                console.warn('Error en la API web, conectando a la API local...', error);
+                return this.http.get<any>(this.apiLocal);
+            })
+        );
+    }
+
+
 }
 
- 
+
+
+
+
+
+
+
+
+
+
+
+// getListProducts():Observable<any>{
+    //     const url ='http://127.0.0.1:8000/productos/v1/products/'
+        
+    //     return this.http.get<any>(url)
+    // } 
+ // 'http://127.0.0.1:8000/productos/v1/products/'
+        // 'https://djangoapi-2xnu.onrender.com/productos/v1/products/'
